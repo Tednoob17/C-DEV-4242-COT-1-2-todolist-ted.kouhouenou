3e49d60c3b95223d248cd78d955b69281cf849bf
Ted Kouhouenou <ted.kouhouenou@epitech.eu>
2024-08-16 08:02:11 +0100

    Add store and completed note features

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,95 @@
 <script setup>
-import { ref } from 'vue';
-import { useTodoStore } from './stores/todos';
+import { ref } from "vue";
+import { useTodoStore } from "./stores/todos";
 
-const store = useTodoStore()
-var td = ref("")
-
+const store = useTodoStore();
+var ttl = ref("");
+var ctn = ref("");
 </script>
 
 <template>
-  <form>
-    Content of todolist
-    <ul>
-      <li v-for="(todo, index) in store.todos" :key="todo">
-        {{ todo }}
-        <button @click="store.deletecontent(index)">Delete</button>
-        <button @click="store.editcontent(index)">Edit</button>
-      </li>
-    </ul>
+  <div>
+    <div
+      class="flex flex-col bg-white w-72 h-48 rounded-md py-4 px-6 border"
+      v-for="(todo, index) in store.todos"
+      :key="index"
+    >
+      <h3 class="text-center font-bold text-xl text-gray-800 pb-2">
+        {{ todo.title }}
+      </h3>
+      <p class="text-sm text-gray-500 pb-3">{{ todo.content }}</p>
+      <div class="flex gap-2 text-sm text-gray-500 border-b pb-2">
+        <p class="">last update:</p>
+        <p>{{ new Date(todo.date) }}</p>
+      </div>
+      <div class="flex justify-around items-center py-3">
+        <div
+          class="flex gap-2 text-gray-600 hover:scale-110 duration-200 hover:cursor-pointer"
+        >
+          <svg
+            class="w-6 stroke-green-700"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path
+              d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"
+            ></path>
+            <path
+              d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"
+            ></path>
+          </svg>
+          <button
+            class="font-semibold text-sm text-green-700"
+            @click="store.editcontent(store.todos.id)"
+          >
+            Edit
+          </button>
+        </div>
+        <div
+          class="flex gap-2 text-gray-600 hover:scale-110 duration-200 hover:cursor-pointer"
+        >
+          <svg
+            class="w-6 stroke-red-700"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <polyline points="3 6 5 6 21 6"></polyline>
+            <path
+              d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"
+            ></path>
+            <line x1="10" y1="11" x2="10" y2="17"></line>
+            <line x1="14" y1="11" x2="14" y2="17"></line>
+          </svg>
+          <button
+            class="font-semibold text-sm text-red-700"
+            @click="store.deletecontent(todo)"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
 
     <br />
     <br />
     <br />
 
-    <label> Input </label> <br />
-    <input v-model="td" type="text" @keyup.enter="store.addcontent(td)" /> <br />
-    <button type="button" @click="store.addcontent(td)">Add Input</button>
-  </form>
+    <form action="" @submit.prevent>
+      <label> Title </label> <br />
+      <input v-model="ttl" type="text" /> <br />
+      <label> Content </label> <br />
+      <input v-model="ctn" type="text" /> <br />
+      <button type="button" @click="store.addcontent(ttl, ctn)">
+        Add Input
+      </button>
+    </form>
+  </div>
 </template>
--- src/stores/todos.js
+++ src/stores/todos.js
@@ -1,25 +1,62 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import { v4 as uuidv4 } from "uuid";
 
-export const useTodoStore = defineStore('todo', () => {
-  const todos = ref([])
-  
-  function addcontent(td) {
-    if (td.trim() !== '' || td === null) {
-      todos.value.push(td)
-      console.log(todos)
+export const useTodoStore = defineStore("todo", () => {
+    const todos = ref([{}]);
+
+    function addcontent(ttl, ctn) {
+      console.log(todos);
+      if (ttl.trim() !== "" ||
+        ctn.trim() !== "" ||
+        ctn !== null ||
+        ttl !== null
+      ) {
+        todos.value.push({
+          id: uuidv4(),
+          title: ttl.substring(0, 15),
+          content: ctn.substring(0, 20),
+          completed: false,
+          date: Date.now(),
+        });
+        console.log(todos);
+      }
+      localStorage.setItem("todos", JSON.stringify(todos.value));
     }
-  }
-  function deletecontent(index) {
-    todos.value.splice(index, 1)
-    console.log(todos)
-  }
-  
-  function editcontent(index) {
-    const edited = prompt('Edit this todo : ')
-    if (edited.trim() !== '' && edited != null) todos.value[index] = edited
-    console.log(todos.value)
-  }
 
-  return { todos, addcontent, deletecontent, editcontent }
-})
+    function deletecontent(todo) {
+      var index = todos.value.indexOf(
+        todos.value.find((value) => {
+          value.id == todo.id;
+        })
+      );
+
+      todos.value.splice(index, 1);
+
+      localStorage.setItem("todos", JSON.stringify(todos.value));
+      console.log(todos.value);
+    }
+
+    function completed(index) {
+      todos[index].completed.value = !todos[index].completed.value;
+      console.log(todos[index].completed.value);
+      localStorage.setItem("todos", JSON.stringify(todos.value));
+    }
+
+    function editcontent(todo) {
+      console.log(todos.value);
+
+      const title = prompt("Edit this todo : ");
+      const console = prompt("Content");
+      if (edited.trim() !== "" && edited != null || console !== null) todos.value = edited.trim();
+      todo.value.title = title;
+      todo.value.content = console; 
+      console.log(todos.value);
+    }
+
+    return { todos, addcontent, deletecontent, editcontent, completed };
+  },
+  {
+    persist: true,
+  }
+);
